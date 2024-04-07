92dcaec36d549942d81ce536125f2eeb5aa12e3b
mayren777 <164793285+mayren777@users.noreply.github.com>
2024-04-06 22:07:34 -0400

    Update May.py

--- May.py
+++ May.py
@@ -6,6 +6,7 @@ def onAppStart(app):
     app.width=1000
     app.height=800
     app.backgroundImage = Image.open("image/background1.jpeg")
+    app.backgroundImage = CMUImage(app.backgroundImage)
     
 
 def redrawAll(app):
@@ -14,7 +15,7 @@ def redrawAll(app):
 
 def drawPage1(app):
     # app.image = Image.open("images/Caaaaat.jpg")
-    drawImage(app.backgroundImage,200,200,width=100,height=100)
+    drawImage(app.backgroundImage,0,0, width = 1000, height = 800)
     # textBox1 = CMUImage(Image.open('textbox1'))
     # drawImage(textBox1, 300, 300, width = 100, height = 100)
     
