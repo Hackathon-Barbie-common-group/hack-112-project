905357b4a88b8076ce410a5453255d5c1e9aee4a
Zinan5 <zinano@andrew.cmu.edu>
2024-04-06 20:45:54 -0400

    Update Ozil.py

--- Ozil.py
+++ Ozil.py
@@ -1,1 +1,17 @@
-Ozil
+from cmu_graphics import *
+
+def onAppStart(app):
+    app.width=800
+    app.height=600
+
+def redrawAll(app): 
+    drawRect(100,100,100,100,fill='black')
+
+    
+
+
+
+def main():
+    runApp()
+
+main()
