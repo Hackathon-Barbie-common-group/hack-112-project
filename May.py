af230fb5232cbfe4d103ecf64a4de7d2d96857c2
mayren777 <164793285+mayren777@users.noreply.github.com>
2024-04-06 22:31:53 -0400

    Update May.py

--- May.py
+++ May.py
@@ -7,6 +7,10 @@ def onAppStart(app):
     app.height=800
     app.backgroundImage = Image.open("image/background1.jpeg")
     app.backgroundImage = CMUImage(app.backgroundImage)
+    app.sue = Image.open("photo/10.PNG")
+    app.sue = CMUImage(app.sue)
+    app.textBox1 = Image.open("image/textbox1.png")
+    app.textBox1 = CMUImage(app.textBox1)
     
 
 def redrawAll(app):
@@ -14,10 +18,12 @@ def redrawAll(app):
     # drawBackground1(app)
 
 def drawPage1(app):
-    # app.image = Image.open("images/Caaaaat.jpg")
+    
     drawImage(app.backgroundImage,0,0, width = 1000, height = 800)
-    # textBox1 = CMUImage(Image.open('textbox1'))
-    # drawImage(textBox1, 300, 300, width = 100, height = 100)
+    drawImage(app.sue, 100, 300, width = 200, height = 500)
+    drawImage(app.textBox1, 200, 300)
+    # drawLabel('112 is so hard, I wish that I could have a date.', 200, 300, fill = 'black', size = 50)
+    # drawRect(200, 300, )
     
     
 
