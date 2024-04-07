057c9a4c7dfb227c638fc8a3e02679a322d699d2
mayren777 <164793285+mayren777@users.noreply.github.com>
2024-04-07 01:03:17 -0400

    adding

--- May.py
+++ May.py
@@ -5,7 +5,9 @@ import os, pathlib
 def onAppStart(app): 
     app.width=1000
     app.height=800
-    app.opening=False
+    app.page1=False
+    app.page2=False
+    app.page3=True
     app.backgroundImage = Image.open("image/background1.jpeg")
     app.backgroundImage = CMUImage(app.backgroundImage)
     app.sue = Image.open("photo/10.PNG")
@@ -18,13 +20,17 @@ def onAppStart(app):
     # app.textBox = CMUImage(app.textBox)
     app.coffeeBackground = Image.open('photo/coffeeBackground')
     app.coffeeBackground = CMUImage(app.coffeeBackground)
+    app.sueFinal = Image.open('photo/sue finals/PNG')
+    app.sueFinal = CMUImage(app.sueFinal)
     
     
 def redrawAll(app):
-    if app.opening:
+    if app.page1:
         drawPage1(app)
-    else: 
+    elif app.page2: 
         drawPage2(app)
+    elif app.page3: 
+        drawPage3(app)
     
    
 
@@ -62,9 +68,24 @@ def drawPage2(app):
     for i in range (len(lines)):
         line = lines[i]
         drawLabel(line, 500, 350 + i + i * 30, size = 15, fill = 'black', font= 'monospace')
+    
+    drawRect(700, 650, 200, 100, fill = 'pink', opacity = 80, border = 'lightCoral', borderWidth = 10)
+    drawLabel('Continue', 800, 700, fill = 'white', size = 30)
+
+def drawPage3(app): 
+    drawImage(app.sueFinals, 100, 300, width = 200, height = 500)
+
+
+
+
 def onMousePress(app,mouseX,mouseY): 
     if 700<=mouseX<=900 and 650<=mouseY<=750: 
-        app.opening=False
+        if app.page1: 
+            app.page1=False
+        elif app.page2: 
+            app.page2=False
+        elif app.page3: 
+            app.page3=False
 
 def main():
     runApp()
